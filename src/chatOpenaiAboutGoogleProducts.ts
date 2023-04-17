f4ad007bb8312c2495d08b0ba3e3581abb88871d
Jonathan Milgrom <jmilgrom@snapchat>
2023-04-17 07:01:56 -0700

    add internal orchestrate

--- src/chatOpenaiAboutGoogleProducts.ts
+++ src/chatOpenaiAboutGoogleProducts.ts
@@ -4,6 +4,11 @@ import { openAiSelectCategoryFromChoices } from "./openai";
 type Chat = { prompt: string; response: string };
 type ChatMetadata = { transcript: Queue<Chat>; model: string; temperature: number };
 
+type Result =
+  | { type: "success"; categories: Queue<string>; metadata: ChatMetadata }
+  | { type: "error:chat"; category: string; metadata: ChatMetadata }
+  | { type: "error:purge"; categories: Queue<string>; metadata: ChatMetadata };
+
 /**
  * Converse with openai traversing the product taxonomy tree for the next multiple choice question
  *
@@ -20,11 +25,9 @@ export const chatOpenaiAboutGoogleProducts = async (
     model = "text-davinci-003",
     temperature = 0.6,
   }: { retries?: number; model?: string; temperature?: number } = {}
-): Promise<
-  | { type: "success"; categories: Queue<string>; metadata: ChatMetadata }
-  | { type: "error:chat"; category: string; metadata: ChatMetadata }
-  | { type: "error:purge"; categories: Queue<string>; metadata: ChatMetadata }
-> => {
+): Promise<Result> => {
+  const transcript = makeQueue<Chat>();
+
   /**
    * (No choices left? We're done! We've found the product category heirarchy.)
    *  1. Get next choices (from found node's children or root product taxonomy)
@@ -34,59 +37,58 @@ export const chatOpenaiAboutGoogleProducts = async (
    *  5. Retries > 0, go to 1 and try again w/o the failed path in the product taxonomy
    *  6. No retries left, we're done, and we haven't found the product category heirarchy
    */
-  let choices: Vertices<string> = productTaxonomy;
-  const categories = makeQueue<string>();
-  const backtrackablePath = makeStack<string>();
-  const transcript = makeQueue<Chat>();
-  while (choices.length) {
-    const {
-      category,
-      metadata: { prompt, response },
-    } = await openAiSelectCategoryFromChoices(toList(choices), webPageMetaData, {
-      model,
-      temperature,
-    });
-    categories.enqueue(category);
-    backtrackablePath.push(category);
-    transcript.enqueue({ prompt, response });
+  const orchestrate = async (retries: number): Promise<Result> => {
+    let choices: Vertices<string> = productTaxonomy;
+    const categories = makeQueue<string>();
+    const backtrackablePath = makeStack<string>();
+    while (choices.length) {
+      const {
+        category,
+        metadata: { prompt, response },
+      } = await openAiSelectCategoryFromChoices(toList(choices), webPageMetaData, {
+        model,
+        temperature,
+      });
+      categories.enqueue(category);
+      backtrackablePath.push(category);
+      transcript.enqueue({ prompt, response });
 
-    const node = find(choices, { path: makeQueue([category]) });
+      const node = find(choices, { path: makeQueue([category]) });
 
-    if (node) {
-      choices = node.children;
-      continue;
+      if (node) {
+        choices = node.children;
+        continue;
+      }
+
+      /**
+       * No retries left and we haven't been able to find an appropriate product category.
+       * This means that either the website is not really a product or that the openai model we chose failed to categorize
+       * the metadata we scraped.
+       */
+      if (!retries) {
+        return { type: "error:chat", category, metadata: { transcript, model, temperature } };
+      }
+
+      console.log("Attempting a different path entirely");
+
+      /**
+       * last node not found and we have some retries left, so we pop it out and purge the product taxonomy of the whole
+       * path and try again.
+       * */
+      backtrackablePath.pop();
+      const ok = purge(productTaxonomy, { path: makeQueue(backtrackablePath.toList()) });
+
+      /**
+       * Something went wrong with the purge util above. This is likely a developer error.
+       */
+      if (!ok) {
+        return { type: "error:purge", categories, metadata: { transcript, model, temperature } };
+      }
+
+      return orchestrate(retries - 1);
     }
+    return { type: "success", categories, metadata: { transcript, model, temperature } };
+  };
 
-    /**
-     * No retries left and we haven't been able to find an appropriate product category.
-     * This means that either the website is not really a product or that the openai model we chose failed to categorize
-     * the metadata we scraped.
-     */
-    if (!retries) {
-      return { type: "error:chat", category, metadata: { transcript, model, temperature } };
-    }
-
-    console.log("Attempting a different path entirely");
-
-    /**
-     * last node not found and we have some retries left, so we pop it out and purge the product taxonomy of the whole
-     * path and try again.
-     * */
-    backtrackablePath.pop();
-    const ok = purge(productTaxonomy, { path: makeQueue(backtrackablePath.toList()) });
-
-    /**
-     * Something went wrong with the purge util above. This is likely a developer error.
-     */
-    if (!ok) {
-      return { type: "error:purge", categories, metadata: { transcript, model, temperature } };
-    }
-
-    return chatOpenaiAboutGoogleProducts(productTaxonomy, webPageMetaData, {
-      retries: retries - 1,
-      model,
-      temperature,
-    });
-  }
-  return { type: "success", categories, metadata: { transcript, model, temperature } };
+  return orchestrate(retries);
 };

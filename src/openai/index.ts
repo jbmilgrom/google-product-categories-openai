375a85ea736b555d4a8e784cab25c5517a999f64
Jonathan Milgrom <jmilgrom@snapchat>
2023-04-22 15:39:17 -0700

    clean up

--- src/openai/index.ts
+++ src/openai/index.ts
@@ -98,9 +98,6 @@ export const generateChatPrompt = (choices: string[], metaTags: string): ChatCom
     Question: Which product category best describes the metadata?
 
     metadata:
-    <meta charset="utf-8">
-    <meta http-equiv="x-dns-prefetch-control" content="on">
-    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
     <meta name="description" content="Buy Kitchen Torch,Cooking Propane Blow Torch Lighter,700,000BTU Flamethrower Fire Gun,Food Culinary Torch with Safety Lock,Campfire Starter Grill Torch,BBQ Torch for Steak &amp; Creme Brulee: Cooking Torches - Amazon.com ✓ FREE DELIVERY possible on eligible purchases">
     <meta name="title" content="Amazon.com: Kitchen Torch,Cooking Propane Blow Torch Lighter,700,000BTU Flamethrower Fire Gun,Food Culinary Torch with Safety Lock,Campfire Starter Grill Torch,BBQ Torch for Steak &amp; Creme Brulee : Home &amp; Kitchen">
 
@@ -162,9 +159,6 @@ export const generateCategorizationAuditChatPrompt = (
     role: "user",
     content: `
       Product metadata:
-      <meta charset="utf-8">
-      <meta http-equiv="x-dns-prefetch-control" content="on">
-      <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
       <meta name="description" content="Amazon.com: Produce Red Mango, 1 Each : Grocery &amp; Gourmet Food">
       <meta name="title" content="Amazon.com: Produce Red Mango, 1 Each : Grocery &amp; Gourmet Food">
 
@@ -178,23 +172,9 @@ export const generateCategorizationAuditChatPrompt = (
     role: "user",
     content: `
       Product metadata:
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0">
       <meta name="description" content="Find the Nike Pegasus 40 Women's Road Running Shoes at Nike.com.  Free delivery and returns.">
-      <meta name="keywords" content="Nike Pegasus 40 Women's Road Running Shoes">
-      <meta name="robots" content="index, follow">
       <meta property="og:title" content="Nike Pegasus 40 Women's Road Running Shoes. Nike.com">
       <meta property="og:description" content="Find the Nike Pegasus 40 Women's Road Running Shoes at Nike.com.  Free delivery and returns.">
-      <meta property="og:type" content="website">
-      <meta property="og:url" content="https://www.nike.com/t/pegasus-40-womens-road-running-shoes-bF2QL9/DV3854-102">
-      <meta property="og:image" content="https://static.nike.com/a/images/t_default/3a487dc8-1548-4e91-8d00-0ebdfd7fe26a/pegasus-40-womens-road-running-shoes-bF2QL9.png">
-      <meta property="og:locale" content="en_US">
-      <meta property="og:site_name" content="Nike.com">
-      <meta property="og:price:currency" content="USD">
-      <meta property="og:price:amount" content="130">
-      <meta name="branch:deeplink:$deeplink_path" content="x-callback-url/product-details?style-color=DV3854-102">
-      <meta property="fb:app_id" content="1397260880304609">
-      <meta name="next-head-count" content="20">
 
       Category: Track & Field
 

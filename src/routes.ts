ab930e891b5f3e1a196f4a9c844f90265ca56761
Jonathan Milgrom <jmilgrom@snapchat>
2023-04-24 15:36:54 -0700

    clean up

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,13 @@
 export const ROUTES = {
-  TEXT: { url: "/text", description: "The Google Product Categories (Text File)" },
+  TEXT: { url: "/text", description: "Text File" },
   INTERNAL_REPRESENTATION: {
     url: "/internal-representation.json",
-    description: "The Google Product Categories (Intenral Representation/JSON)",
+    description: "JSON (Intenral Representation)",
   },
   MAX_DEPTH: { url: "/max-depth", description: "Calculate Max Depth" },
   MAX_DEGREE: { url: "/max-degree", description: "Calculate Max Degree" },
   SEARCH: { url: "/search", description: "Search (Substring Matching Only)" },
-  TRAVERSE: { url: "/traverse", description: "Explore the Graph of Google Product Categories" },
+  TRAVERSE: { url: "/traverse", description: "Explore" },
   URL: { url: "/url", description: "Categorize a URL using Google Product Categories" },
 } as const;
 

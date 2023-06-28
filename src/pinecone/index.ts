c535b1685b16931ed04ebc74558f7865ceb25b27
Jonathan Milgrom <jmilgrom@snapchat>
2023-06-28 15:54:11 -0400

    scaffold store

--- src/pinecone/index.ts
+++ src/pinecone/index.ts
@@ -1,14 +1,15 @@
 import { PineconeClient } from "@pinecone-database/pinecone";
+import { VectorOperationsApi } from "@pinecone-database/pinecone/dist/pinecone-generated-ts-fetch";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const { ENVIRONMENT, API_KEY } = process.env;
+const { PINECONE_ENVIRONMENT, PINECONE_API_KEY } = process.env;
 
-if (ENVIRONMENT === undefined) {
-  throw new Error("Pinecone ENVIRONMENT is undefined.");
+if (PINECONE_ENVIRONMENT === undefined) {
+  throw new Error("PINECONE_ENVIRONMENT is undefined.");
 }
-if (API_KEY === undefined) {
+if (PINECONE_API_KEY === undefined) {
   throw new Error("Pinecone API_KEY is undefined.");
 }
 
@@ -16,9 +17,24 @@ export const initializePineconeClient = async (): Promise<PineconeClient> => {
   const pinecone = new PineconeClient();
 
   await pinecone.init({
-    environment: ENVIRONMENT,
-    apiKey: API_KEY,
+    environment: PINECONE_ENVIRONMENT,
+    apiKey: PINECONE_API_KEY,
   });
 
   return pinecone;
+};
+
+export const getOrCreatePineconeIndex = async (
+  pinecone: PineconeClient,
+  { name, dimension }: { name: string; dimension: number }
+): Promise<VectorOperationsApi> => {
+  const indexes = await pinecone.listIndexes();
+
+  if (!indexes.includes(name)) {
+    pinecone.createIndex({
+      createRequest: { name, dimension },
+    });
+  }
+
+  return pinecone.Index(name);
 };

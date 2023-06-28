d4f256e8a6864bd7410ef79f03b32d24110c58b5
Jonathan Milgrom <jmilgrom@snapchat>
2023-06-28 15:54:40 -0400

    scaffold script

---
+++ src/scripts/langchain/populateOpenAiPineconeStore.ts
@@ -0,0 +1,66 @@
+import * as dotenv from "dotenv";
+import { getOrCreatePineconeIndex, initializePineconeClient } from "../../pinecone";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { Document } from "langchain/document";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { GOOGLE_PRODUCT_TYPES_URL, makeGoogleProductTypeTextLineIterator } from "../../googleProducts";
+
+dotenv.config();
+
+const ADA_002_EMBEDDING_MODEL = "text-embedding-ada-002";
+
+/**
+ * For model text-embedding-ada-002
+ *
+ * @see https://openai.com/blog/new-and-improved-embedding-model
+ * */
+const OPEN_AI_TEXT_EMBEDDING_ADA_002_DIMENSION = 1536;
+
+const { PINECONE_INDEX_NAME_OPEN_AI_ADA_002 } = process.env;
+
+if (PINECONE_INDEX_NAME_OPEN_AI_ADA_002 === undefined) {
+  throw new Error("PINECONE_INDEX_NAME_OPEN_AI_ADA_002 is undefined.");
+}
+
+const openAiAda002Embedder = new OpenAIEmbeddings({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  modelName: ADA_002_EMBEDDING_MODEL,
+});
+
+const loadDocuments = async (): Promise<Document[]> => {
+  const documents: Document[] = [];
+  let lineNumber = 0;
+  for await (const line of makeGoogleProductTypeTextLineIterator()) {
+    documents.push(
+      new Document({ pageContent: line, metadata: { source: GOOGLE_PRODUCT_TYPES_URL, id: lineNumber++ } })
+    );
+  }
+  return documents;
+};
+
+(async () => {
+  try {
+    const pineconeClient = await initializePineconeClient();
+
+    console.log("Loading documents.");
+
+    const documents = await loadDocuments();
+
+    console.log("Generating or retrieving Pinecone index.");
+
+    const pineconeIndex = await getOrCreatePineconeIndex(pineconeClient, {
+      name: PINECONE_INDEX_NAME_OPEN_AI_ADA_002,
+      dimension: OPEN_AI_TEXT_EMBEDDING_ADA_002_DIMENSION,
+    });
+
+    console.log("Retrieving embeddings from OpenAI and populating Pinecone index.");
+
+    await PineconeStore.fromDocuments(documents, openAiAda002Embedder, {
+      pineconeIndex,
+    });
+
+    console.log("Successfully populated Pinecone with OpenAI vector embeddings.");
+  } catch (e) {
+    console.log("Error populating Pinecone with OpenAI vector embeddings:", e);
+  }
+})();
